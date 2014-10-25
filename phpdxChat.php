3c26e18ac5b558e335e8de27a6e01a77bed9a73d
Will Vaughn <willieviseoae@gmail.com>
2014-10-25 16:14:12 -0700

    Tailing buffer handler works.

--- phpdxChat.php
+++ phpdxChat.php
@@ -4,6 +4,7 @@ use Monolog\Handler\BufferHandler;
 use Monolog\Logger;
 use Nack\FileParser\FileParser;
 use Nack\Monolog\Handler\GitterImHandler;
+use Nack\Monolog\Handler\TailBufferHandler;
 
 require 'vendor/autoload.php';
 
@@ -11,16 +12,27 @@ $fileParser = new FileParser();
 $config = $fileParser->json(__DIR__ . '/config/config.json');
 
 $gitterHandler = new GitterImHandler($config['gitterToken'], $config['gitterRoomId'], Logger::DEBUG);
-$bufferHandler = new BufferHandler($gitterHandler);
+$tailBufferHandler = new TailBufferHandler($gitterHandler, 5);
 
 $logger = new Logger("will-experiments-phpdx.chat");
-$logger->pushHandler($bufferHandler);
+$logger->pushHandler($tailBufferHandler);
 
-$logger->debug('A gitter.im debug monolog', ['ctx' => 'minutia']);
-$logger->info('A gitter.im info monolog', ['ctx' => 'interesting']);
-$logger->notice('A gitter.im notice monolog', ['ctx' => 'significant']);
-$logger->warning('A gitter.im warning monolog', ['ctx' => 'disruptive']);
-$logger->error('A gitter.im error monolog', ['ctx' => 'error']);
+// These will get left out
+$logger->debug('debug outside buffer capacity');
+$logger->warning('warning outside buffer capacity');
+$logger->info('info outside buffer capacity');
+
+// These will make it
+$logger->debug('debug in buffer', ['ctx' => 'minutia']);
+$logger->info('info in buffer', ['ctx' => 'interesting']);
+$logger->notice('notice in buffer', ['ctx' => 'significant']);
+$logger->error('error in buffer', ['ctx' => 'error']);
+
+// This critical triggers the tail buffer handler to flush.
+// Buffer capacity is set purposefully low at 5 to demonstrate how buffer capacity works.
 $logger->critical('A gitter.im critical monolog', ['ctx' => 'investigate']);
-$logger->alert('A gitter.im alert monolog', ['ctx' => 'take action']);
-$logger->emergency('A gitter.im emergency monolog', ['ctx' => 'boom!']);
+
+// The buffer has been reset, here we go for some more.
+$logger->warning('warning in the second batch');
+$logger->info('in the second batch');
+$logger->emergency('emergency triggers another flush', ['ctx' => 'boom!']);
