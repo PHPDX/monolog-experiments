f0be50854213f002f8bed01332aae0c53db25405
Will Vaughn <willieviseoae@gmail.com>
2014-10-25 18:52:49 -0700

    Email does tailing, chat does regular buffering.

--- bufferedEmail.php
+++ bufferedEmail.php
@@ -1,40 +1,49 @@
 <?php
-/**
- * This script is an example of using the monolog BufferHandler with the NativeMailerHandler to send one email which
- * captures all of the logs which ran during the execution of the script. Normal operation of the NativeMailerHandler
- * is to send one email per log. That could get pretty annoying. The buffer handler is set up here to wrap
- * the NativeMailerHandler, but it could be used to wrap any other HandlerInterface.
- *
- * When the php script has finished running, the buffer handler closes itself before destruction and sends
- * them as a batch to the underlying mail handler which uses the built in php mail() function to send a single email
- * of all logs above Logger::ERROR
- */
 
-use Monolog\Handler\BufferHandler;
 use Monolog\Handler\NativeMailerHandler;
 use Monolog\Logger;
+use Nack\Monolog\Handler\TailBufferHandler;
 
 require 'vendor/autoload.php';
 
-$to = "user@domain.com";
+$to = "user@example.com";
 $subject = "Buffered Email Test";
 $from = "monolog.scratch@example.com";
 
-// NativeMailerHandler, set to only care about ERROR+ logs from this script.
-$mailHandler = new NativeMailerHandler($to, $subject, $from, Logger::ERROR);
+$bufferCapacity = 25;
+$mailHandler = new NativeMailerHandler($to, $subject, $from, Logger::NOTICE);
+$tailBufferHandler = new TailBufferHandler($mailHandler, $bufferCapacity, Logger::NOTICE);
 
-// BufferHandler wraps the NativeMailerHandler
-// Is set up with a bufferLimit of 0, which means no limit.
-// And is set up to buffer all log levels DEBUG+
-$bufferHandler = new BufferHandler($mailHandler, 0, Logger::DEBUG);
-
-// Give the buffer handler to the logger.
 $logger = new Logger("will-experiments-buffered.email");
-$logger->pushHandler($bufferHandler);
+$logger->pushHandler($tailBufferHandler);
 
-$logger->debug("You won't get this in the email");
-$logger->info("Won't get this either");
-$logger->notice("Or this, but you will get the next 3 in a single email.");
-$logger->error("error, coming in a single buffered email");
-$logger->critical("critical, coming in a single buffered email");
-$logger->emergency("emergency, coming in a single buffered email");
+
+$logger->debug('test.debug will not be in emails or buffers');
+
+for ($i = 0; $i < ($bufferCapacity - 1); $i++) {
+    $logger->notice('test.notice');
+}
+
+$logger->critical('test.critical');
+
+for ($i = 0; $i < 5; $i++) {
+    $logger->warning('test.warning');
+}
+
+$logger->info('test.info will not be in emails or buffers');
+
+$logger->emergency('test.emergency');
+
+// Wont see these next two, they're put in buffer, but never flushed by a CRITICAL+
+$logger->notice('test.notice');
+$logger->error('test.notice');
+
+// =========================== EMAIL #1 ==========================================
+// 1 CRITICAL + 24 NOTICE in an email.
+// DEBUG below the lowest buffered level, ignored.
+// ===============================================================================
+
+// =========================== EMAIL #2 ==========================================
+// 1 EMERGENCY + 5 WARNING + 1 CRITICAL + 17 NOTICE in an email
+// INFO below the lowest buffered level, ignored.
+// ===============================================================================
--- phpdxChat.php
+++ phpdxChat.php
@@ -4,32 +4,20 @@ use Monolog\Handler\BufferHandler;
 use Monolog\Logger;
 use Nack\FileParser\FileParser;
 use Nack\Monolog\Handler\GitterImHandler;
-use Nack\Monolog\Handler\TailBufferHandler;
 
 require 'vendor/autoload.php';
 
 $fileParser = new FileParser();
 $config = $fileParser->json(__DIR__ . '/config/config.json');
 
-$bufferCapacity = 25;
-$gitterHandler = new GitterImHandler($config['gitterToken'], $config['gitterRoomId'], Logger::DEBUG);
-$tailBufferHandler = new TailBufferHandler($gitterHandler, $bufferCapacity);
+$gitterHandler = new GitterImHandler($config['gitterToken'], $config['gitterRoomId']);
+$tailBufferHandler = new BufferHandler($gitterHandler);
 
 $logger = new Logger("will-experiments-phpdx.chat");
 $logger->pushHandler($tailBufferHandler);
 
-for ($i = 0; $i < ($bufferCapacity - 1); $i++) {
-    $logger->debug('test.debug');
-}
-
-// This is going to make a message of the critical log +24 debugs.
-$logger->critical('test.critical');
-
-for ($i = 0; $i < 5; $i++) {
-    $logger->notice('test.notice');
-}
-
-// This is going to make a message of this emergency, 5 notices, a critical, and 17 debugs.
+$logger->debug('test.debug ignored.');
+$logger->critical('test.critical in gitter.im', ['context' => 'here']);
+$logger->notice('test.notice');
 $logger->emergency('test.emergency');
-
-// Make sense?
+$logger->error('test.error');
