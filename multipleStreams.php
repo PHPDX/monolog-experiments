9ea077f8a78defc84bf0d2bc5afa7c60f27cac45
Will Vaughn <willieviseoae@gmail.com>
2014-10-28 23:21:14 -0700

    Added a really contrived example to exhibit the IntrospectionProcessor.

--- multipleStreams.php
+++ multipleStreams.php
@@ -2,18 +2,26 @@
 
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
+use Monolog\Processor\IntrospectionProcessor;
 
 require 'vendor/autoload.php';
 
-// So basic
-$stdoutHandler = new StreamHandler('php://stdout');
-$errorFileHandler = new StreamHandler(__DIR__ . '/multiple.streams.log', Logger::WARNING);
+// Wrapping in a function to exhibit the IntrospectionProcessor
+function traceMe()
+{
+    // So basic
+    $stdoutHandler = new StreamHandler('php://stdout');
+    $errorFileHandler = new StreamHandler(__DIR__ . '/multiple.streams.log', Logger::WARNING);
 
-$logger = new Logger('will-experiments-multiple.streams');
-$logger->pushHandler($stdoutHandler);
-$logger->pushHandler($errorFileHandler);
+    $logger = new Logger('will-experiments-multiple.streams');
+    $logger->pushHandler($stdoutHandler);
+    $logger->pushHandler($errorFileHandler);
+    $logger->pushProcessor(new IntrospectionProcessor());
 
-$logger->debug('look context data', ['foo' => 'bar']);
-$logger->info('you will see this is stdout, but not the file');
-$logger->warning('warning goes to stdout and the file');
-$logger->error('error is handled by both handlers as well', ['serious' => 'error']);
+    $logger->debug('look context data', ['foo' => 'bar']);
+    $logger->info('you will see this is stdout, but not the file');
+    $logger->warning('warning goes to stdout and the file');
+    $logger->error('error is handled by both handlers as well', ['serious' => 'error']);
+}
+
+traceMe();
