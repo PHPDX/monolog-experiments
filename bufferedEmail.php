e713fcd3569a21ce1382dbd76ab69241b79b8f6e
Will Vaughn <willieviseoae@gmail.com>
2014-10-28 23:00:08 -0700

    doc comments.

--- bufferedEmail.php
+++ bufferedEmail.php
@@ -26,11 +26,13 @@ $logger->debug('test.debug will not be in emails or buffers');
 for ($i = 0; $i < ($bufferSize - 1); $i++) {
     $logger->notice('test.notice');
 }
+// This critical log activates a handling of the FingersCrossed buffer.
 $logger->critical('test.critical');
 
 ////////////////// EMAIL 2 ////////////////////////////////////
 $logger->warning('test.warning');
 $logger->info('test.info will not be in emails or buffers');
+// This emergency log activates a second handling of the FingersCrossed buff
 $logger->emergency('test.emergency');
 
 // Wont see these next two, they're put in buffer, but no CRITICAL+ log triggers them to be sent.
